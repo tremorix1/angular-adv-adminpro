573fabcacd2d08a8b925414e0793776945c80605
Carlos <cpreztello@gmail.com>
2020-08-25 14:41:15 -0400

    Fin sección 6

--- src/app/pages/grafica1/grafica1.component.ts
+++ src/app/pages/grafica1/grafica1.component.ts
@@ -1,16 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { DonaComponent } from '../components/dona/dona.component';
 @Component({
   selector: 'app-grafica1',
   templateUrl: './grafica1.component.html',
   styles: [
   ]
 })
-export class Grafica1Component implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class Grafica1Component {
+public labels1: string[] = ['Pan', 'Refrescos', 'Burritos'];
+public data1 = [
+  [10, 15, 40],
+];
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from "@angular/forms";
+
+// Modulos
+import { SharedModule } from '../shared/shared.module';
+import { ComponentsModule } from "../components/components.module";
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
@@ -22,8 +26,11 @@ import { PagesComponent } from './pages.component';
     PagesComponent,
   ],
   imports: [CommonModule,
+    FormsModule,
   SharedModule,
-  RouterModule
+  RouterModule,
+  ComponentsModule
+
 ]
 })
 export class PagesModule { }
